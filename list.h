37dc7e6cdf5b30240d07710eb2f15b85a3b99f7d
jorgemayna <jorgemayna@gmail.com>
2018-08-28 19:41:23 -0500

    avanze de lista

--- list.h
+++ list.h
@@ -5,6 +5,7 @@
 #include "node.h"
 #include "iterator.h"
 
+
 using namespace std;
 
 template <typename T>
@@ -19,8 +20,8 @@ class List {
     public:
         List();
 
-        int front();
-        int back();
+        T front();
+        T back();
         void push_front(T value);
         void push_back(T value);
         void pop_front();
@@ -41,19 +42,209 @@ class List {
 template<class T> List<T>::List()
 {
 
+    head=NULL;
+    tail=NULL;
+
+    nodes=0;
+
 }
-template<class T> List<T>::~List()
+
+
+template<typename T> T List<T>::front()
 {
 
+    return(head->data);
+
+}
+
+template<typename T> T List<T>::back()
+{
+
+    return(tail->data);
+
+}
+
+template<typename T> int List<T>::size()
+{
+    return nodes;
+}
+
+template<typename T> bool List<T>::empty()
+{
+    bool a=true;
+    if(head==NULL)
+    {
+        return a;
+    }else{
+        return !a;
+    }
+
+}
+
+template<typename T> void List<T>::push_front(T value)
+{
+    Node<T>* temp = new Node<T>;
+    temp->data=value;
+
+    if (head==NULL)
+    {
+
+        tail=temp;
+
+    }
+    temp->next=head;
+    head=temp;
+    nodes++;
+    temp=NULL;
+    delete temp;
+
+
+
+}
+template<typename T> void List<T>::pop_front()
+{
+    if(head!=tail)
+    {
+        Node<T>* temp=head;
+
+        temp=head->next;
+        head=NULL;
+        delete head;
+        head=temp;
+        temp=NULL;
+        delete temp;
+
+
+    }else{
+        head=NULL;
+        tail=NULL;
+        delete head;
+        delete tail;
+    }
+}
+template<typename T> T List<T>::get(int position)
+{
+    Node<T>* temp=head;
+    T rpta;
+    for(int x=0;x!=position;x++)
+    {
+
+        rpta=temp->data;
+        temp=temp->next;
+    }
+    temp= NULL;
+    delete temp;
+    return rpta;
+
+}
+
+template<typename T> void List<T>::pop_back()
+{
+
+    if(head!=tail)
+    {
+        Node<T>* temp=head;
+        while(temp->next!=tail)
+        {
+            temp=temp->next;
+        }
+        temp->next=NULL;
+        tail=NULL;
+        delete tail;
+        tail=temp;
+        temp=NULL;
+        delete temp;
+
+
+    }else{
+        head=NULL;
+        tail=NULL;
+        delete head;
+        delete tail;
+    }
 }
 
 template<typename T> void List<T>::push_back(T value)
 {
-    head->data=value;
+    //  INCOMPLETO
+    Node<T>* temp = new Node<T>;
+    temp->data=value;
+    temp->next=NULL;
+    nodes++;
+    if (head==NULL)
+    {
+
+        head=temp;
+    }else{
+
+        tail->next=temp;
+    }
+    tail=temp;
+
+    temp=NULL;
+    delete temp;
+
+
 }
+
+template<typename T> void List<T>::clear()
+{
+    if(!empty())
+    {
+        while(head!=tail)
+        {
+            pop_front();
+
+        }
+        head=NULL;
+        delete head;
+
+    }
+
+
+
+}
+
 
 template<typename T> void List<T>::print()
 {
-    cout << head->data;
+    Node<T>* temp=new Node<T>;
+    temp=head;
+    while(temp!=tail)
+    {
+        cout<<temp->data<<endl;
+        temp=temp->next;
+
+    }
+    cout << tail->data<<endl;
+    temp=NULL;
+    delete temp;
+}
+template<typename T> void List<T>::print_reverse()
+{
+    T lista[nodes];
+    Node<T>* temp=head;
+    int aux=0;
+    while(temp!=tail)
+    {
+        //cout<<temp->data<<endl;
+        lista[aux]=temp->data;
+        aux++;
+        temp=temp->next;
+
+    }
+    //cout << tail->data<<endl    ;
+    temp=NULL;
+    delete temp;
+
+    for(int x=nodes;0<x;x--)
+    {
+        cout << lista[x-1] << endl;
+    }
+}
+template<class T> List<T>::~List()
+{
+    clear();
+
 }
 #endif
